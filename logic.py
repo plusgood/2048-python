ef3c31acee693092f347185c002ada49f25fe62d
Sanjeev Murty <murty.sanjeev@gmail.com>
2015-02-01 19:46:32 -0700

    Changed range to xrange in logic.py

--- logic.py
+++ logic.py
@@ -19,7 +19,7 @@ from random import *
 def new_game(n):
     matrix = []
 
-    for i in range(n):
+    for i in xrange(n):
         matrix.append([0] * n)
     return matrix
 
@@ -54,22 +54,22 @@ def add_two(mat):
 # 3 marks for correct checking
 
 def game_state(mat):
-    """for i in range(len(mat)):
-        for j in range(len(mat[0])):
+    """for i in xrange(len(mat)):
+        for j in xrange(len(mat[0])):
             if mat[i][j]==2048:
                 return 'win'"""
-    for i in range(len(mat)-1): #intentionally reduced to check the row on the right and below
-        for j in range(len(mat[0])-1): #more elegant to use exceptions but most likely this will be their solution
+    for i in xrange(len(mat)-1): #intentionally reduced to check the row on the right and below
+        for j in xrange(len(mat[0])-1): #more elegant to use exceptions but most likely this will be their solution
             if mat[i][j]==mat[i+1][j] or mat[i][j+1]==mat[i][j]:
                 return 'not over'
-    for i in range(len(mat)): #check for any zero entries
-        for j in range(len(mat[0])):
+    for i in xrange(len(mat)): #check for any zero entries
+        for j in xrange(len(mat[0])):
             if mat[i][j]==0:
                 return 'not over'
-    for k in range(len(mat)-1): #to check the left/right entries on the last row
+    for k in xrange(len(mat)-1): #to check the left/right entries on the last row
         if mat[len(mat)-1][k]==mat[len(mat)-1][k+1]:
             return 'not over'
-    for j in range(len(mat)-1): #check up/down entries on last column
+    for j in xrange(len(mat)-1): #check up/down entries on last column
         if mat[j][len(mat)-1]==mat[j+1][len(mat)-1]:
             return 'not over'
     return 'lose'
@@ -86,9 +86,9 @@ def game_state(mat):
 
 def reverse(mat):
     new=[]
-    for i in range(len(mat)):
+    for i in xrange(len(mat)):
         new.append([])
-        for j in range(len(mat[0])):
+        for j in xrange(len(mat[0])):
             new[i].append(mat[i][len(mat[0])-j-1])
     return new
 
@@ -104,9 +104,9 @@ def reverse(mat):
 
 def transpose(mat):
     new=[]
-    for i in range(len(mat[0])):
+    for i in xrange(len(mat[0])):
         new.append([])
-        for j in range(len(mat)):
+        for j in xrange(len(mat)):
             new[i].append(mat[j][i])
     return new
 
@@ -126,9 +126,9 @@ def transpose(mat):
 def cover_up(mat):
     new=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     done=False
-    for i in range(4):
+    for i in xrange(4):
         count=0
-        for j in range(4):
+        for j in xrange(4):
             if mat[i][j]!=0:
                 new[i][count]=mat[i][j]
                 if j!=count:
@@ -138,8 +138,8 @@ def cover_up(mat):
 
 def merge(mat):
     done=False
-    for i in range(4):
-         for j in range(3):
+    for i in xrange(4):
+         for j in xrange(3):
              if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
                  mat[i][j]*=2
                  mat[i][j+1]=0
