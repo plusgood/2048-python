e4d7c5f3cbdde548a79ad18741d919c0a246f366
Sanjeev Murty <murty.sanjeev@gmail.com>
2015-02-01 19:48:18 -0700

    Added 4096 color and default color in puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -14,10 +14,12 @@ BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 BACKGROUND_COLOR_DICT = {   2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", \
                             32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", \
-                            512:"#edc850", 1024:"#edc53f", 2048:"#edc22e" }
+                            512:"#edc850", 1024:"#edc53f", 2048:"#edc22e", 4096: "#00ff00",
+                            8192:"#0000ff"}
 CELL_COLOR_DICT = { 2:"#776e65", 4:"#776e65", 8:"#f9f6f2", 16:"#f9f6f2", \
                     32:"#f9f6f2", 64:"#f9f6f2", 128:"#f9f6f2", 256:"#f9f6f2", \
-                    512:"#f9f6f2", 1024:"#f9f6f2", 2048:"#f9f6f2" }
+                    512:"#f9f6f2", 1024:"#f9f6f2", 2048:"#f9f6f2", 4096 : "#f9f6f2",
+                    8192:"#f9f6f2"}
 FONT = ("Verdana", 40, "bold")
 
 KEY_UP_ALT = "\'\\uf700\'"
@@ -91,7 +93,7 @@ class GameGrid(Frame):
                 if new_number == 0:
                     self.grid_cells[i][j].configure(text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
                 else:
-                    self.grid_cells[i][j].configure(text=str(new_number), bg=BACKGROUND_COLOR_DICT[new_number], fg=CELL_COLOR_DICT[new_number])
+                    self.grid_cells[i][j].configure(text=str(new_number), bg=BACKGROUND_COLOR_DICT.get(new_number, "#ff0000"), fg=CELL_COLOR_DICT.get(new_number, "#f9f6f2"))
         self.update_idletasks()
         
     def key_down(self, event):
